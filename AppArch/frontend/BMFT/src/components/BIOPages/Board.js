a6f3860e28fa8bb241decd15cd82ffdf9d264c93
BMFT Com <communitycoordinator@bmft.org.bz>
2025-07-18 14:55:56 -0600

    Update Board.js

--- AppArch/frontend/BMFT/src/components/BIOPages/Board.js
+++ AppArch/frontend/BMFT/src/components/BIOPages/Board.js
@@ -9,34 +9,40 @@ import Stop from "../assets/Stop.JPG";
 
 const teamMembers = [
   {
-      name: "Elma Kay",
-      position: "Managing Director of the Belize Maya Forest Trust",
-      description: "Elma Kay, Managing Director of the Belize Maya Forest Trust was part of the effort to secure the protection of the Belize Maya Forest. She combines almost 20 years of experience in academics, conservation and policy work for protected areas and forest management, as well as institutional building and organizational leadership for civil society organizations. She has served in committees related to carbon such as the Belize National Climate Change Committee and the Reducing Emissions from Deforestation and Forest Degradation (REDD+) Technical Expert Group, currently serves in several non-governmental organization (NGO) boards and is Chairwoman of the board of directors of the Belize Network of NGO’s.",
-      imgSrc: Elma
+      name: "Deeriann Buckley-Wade",
+      position: "Uknown",
+      description: "Deeriann Buckley-Wade is the Conservation Analyst with The Nature Conservancy’s Belize Business Unit. She holds a dual bachelor’s degree in environmental science from Galen University and the University of Indianapolis, as well as a master’s degree in Climate Change and Development from the SOAS University of London. Prior to TNC, she served as a public officer with the Government of Belize for almost 16 years, working in various capacities within the administrative and technical grades. Most recently, she served as a Blue Economy Officer responsible for matters relating to energy, climate change, maritime transport and sustainable development with the Ministry of Blue Economy and Civil Aviation. ",
+      imgSrc: DBuck
     },
   {
-      name: "Sayuri Tzul",
-      position: "Community Outreach and Stewards Coordinator ",
-      description: "Sayuri Tzul is currently in the role of Community Outreach and Stewards Coordinator, where she brings her extensive experience in wildlife conservation, epiphyte inventory, communications, conservation education, and sustainable tourism. With over a decade of background in conservation work in Belize, she has pursued further education, earning a master's degree in Agrobusiness and Sustainable Markets from the University of CATIE in Costa Rica. Sayuri has been instrumental in developing educational materials for conservation education, implementing effective communication strategies, and initiating stewardship programs, all in line with her dedication to driving positive change and enhancing livelihoods through her work in conservation and sustainability.",
-      imgSrc: Tzul
+      name: "Michael H.M. Bowen",
+      position: "Unkown",
+      description: "Michael H.M. Bowen, an eighth-generation Belizean, who’s family first came to Belize in 1752, was educated in the United States of America, joined the family company after graduating from Duke University. In 2010 Michael became the CEO of Bowen & Bowen, a third-generation family company, which is the parent company of Crystal Bottling Company and Belize Brewing Company, the bottlers of Coca-Cola and Belikin Beer, among other companies, including Gallon Jug, a ~30,000 acres private rainforest jungle reserve which host the world-renown boutique lodge Chan Chich. Michael continues to push Bowen & Bowen’s mission statement, Pursuing excellence for a stronger Belize. ",
+      imgSrc: MichealHM
     },
     {
-      name: "Lyndon Mai",
-      position: "Protected Areas Manager",
-      description: "Lyndon currently serves as the Protected Area Manager for the Belize Maya Forest Trust, where he is committed to the conservation and protection of Belize's natural heritage. He is responsible for overseeing the protection and management of the Belize Maya Forest, a vast expanse of 236,000 acres. His role involves implementing a strategic approach to environmental protection to ensure the forest continued preservation and heath. He began his tenure at BMFT as the Protection and Operations Coordinator, playing a pivotal role in safeguarding the forest and coordinating efforts to maintain its ecological integrity. In his current position, he manages the daily operations of the ranger team, aligning fieldwork and administrative activities with the organization’s conservation objectives. Before joining BMFT, he served for a decade with the Belize Defence Force. During his military career, he developed a diverse skill set and a disciplined approach to operations management. His expertise in navigation, mapping, and operational oversight has proven essential in his present role. Driven by a passion for the natural world and a strong foundation in organizational and technological skills, Lyndon is dedicated to advancing BMFT’s mission. He is enthusiastic about contributing to the protection and sustainability of the Belize Maya Forest, ensuring its preservation for future generations.",
-      imgSrc: Lyndon
+      name: "Justin Pepper",
+      position: "Uknown",
+      description: "Justin Pepper is the Bobolink Foundation’s Chief Conservation Officer. Bobolink’s focus is biodiversity conservation in the Americas, emphasizing grasslands, coastal conservation, wildlife and wild landscapes, and community-based conservation. Prior to Bobolink, Justin worked for Audubon, first, supporting, amplifying and celebrating grassroots conservation leaders in the Chicago area, and later, developing a market-based approach to grassland bird conservation in partnership with conservation-minded ranchers. He previously worked for the Missouri Department of Conservation and The Nature Conservancy of Minnesota and graduated from the University of Kansas. ",
+      imgSrc: JPepper
+    },
+   {
+      name: "Matt Griffis",
+      position: "Past Board Director",
+      description: "Matt Griffis serves as the Conservation Program Officer at the Wyss Foundation, a position he has held since 2011. In his role, he is responsible for managing the Foundation's land acquisitions program and overseeing conservation grantmaking efforts in Africa, Asia, and Europe. He earned a Bachelor of Science degree in Environmental Sciences from the University of California, Berkeley, and went on to obtain a Master of Science degree in Environmental Policy and Planning from the University of Michigan. Matt resides in Durango, Colorado, where he engages in activities such as backpacking, trail running, skiing, and volunteering with organizations like the American Hiking Society and the Boy Scouts of America.",
+      imgSrc: MGriffis
+    },
+  {
+      name: "Mark Gruin",
+      position: "Past Board Director",
+      description: "Past Board Director (Deceased) Mark Gruin, Trustee of Rainforest Trust has dedicated more than 30 years to international conservation and sustainable development. He has worked, lived and traveled in more than 40 countries, helping a wide range of local and international non-governmental organizations create, develop and implement sustainable development strategies that integrate preserving biodiversity and threatened habitats with creating economic and humanitarian benefits to local communities. His career has been characterized by committed early involvement, working on the ground in early stages of difficult projects to find ways to quickly address immediate needs, while establishing a foundation for lasting and sustainable impact and increased local capacity. An inveterate wanderer and wonderer, working in remote places with little infrastructure and pressing needs has been the norm, including many projects throughout South and Central America and the Caribbean, the Russian Far East, sub-Saharan and the Horn of Africa, North America, Europe and the Caucasus. ",
+      imgSrc: MGruin
     },
     {
-      name: "Ermain Requena",
-      position: "Restoration and Regenerative Agriculture Coordinator",
-      description: "Ermain Requena is a proud team member of the Belize Maya Forest Trust, serving as the Restoration and Regenerative Agriculture Coordinator. With over fifteen years of experience in working with farmers and providing technical support for small-scale agriculture, Ermain is dedicated to sharing his knowledge and promoting sustainable farming practices within the farming community. As part of the Belize Maya Forest Trust, Ermain collaborates with nine stakeholder communities in the wider Belize Maya Forest landscape. The focus of their regenerative agriculture program is to enhance livelihoods through nature-based solutions while working closely with farmers, community stakeholders, and community stewards. Ermain is grateful to be part of a passionate and diverse team that is committed to conserving the Belize Maya Forest through collaboration with nearby communities. ",
-      imgSrc: ProfilePic
-    },
-    {
-      name: "Raquel Chun",
-      position: "Conservation Data Analyst",
-      description: "Raquel comes from the beautiful village of Roaring Creek in the Cayo District. She has joined the Belize Maya Forest Trust (BMFT) team as the new Conservation Data Analyst. The endless natural beauty of Belize, along with the struggles and challenges in managing these areas, has always fascinated her. Her journey into conservation started at the University of Belize, where she studied marine science and natural resource management. She furthered her studies abroad with a Master's degree in Wildlife Health and Population Management at the University of Sydney, Australia and she is completing a doctoral degree in Climate Change and Smallholder Agriculture at Durham University in the United Kingdom. Belize has always held her heart and she has returned home to help protect the vibrant natural resources and contribute to the communities that depend on them.",
-      imgSrc: Raquel
+      name: "xxxxxx",
+      position: "Belize",
+      description: "J",
+      imgSrc: JRob
     },
 
    
