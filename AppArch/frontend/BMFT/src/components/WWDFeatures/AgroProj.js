19fa50a63cdbe890718e32487935324d5c0e9904
BMFT Com <communitycoordinator@bmft.org.bz>
2025-07-22 20:46:42 -0600

    Update AgroProj.js

--- AppArch/frontend/BMFT/src/components/WWDFeatures/AgroProj.js
+++ AppArch/frontend/BMFT/src/components/WWDFeatures/AgroProj.js
@@ -54,10 +54,12 @@ const AgroProj = () => {
                 <div className={styles.introText}>
                     <h2>About the Program</h2>
                     <p>
-                        The nine stakeholder communities surrounding the BMF landscape predominantly consist of agricultural communities engaged in the cultivation of vegetables, fruits, and rearing of livestock. These communities primarily employ conventional farming methods characterized by significant reliance on chemical inputs and the practice of monocropping. In response to the challenges posed by these traditional practices, the regenerative agriculture program aims to enhance the livelihoods of farmers while facilitating their transition towards more sustainable agricultural methods. This initiative focuses on implementing restorative soil health practices, including the use of cover crops such as Mucuna and Canavalia beans, as well as Inga edulis, all of which are leguminous plants known to improve soil fertility.
+                        Agriculture and agribusiness play a crucial role in providing income and sustaining the livelihoods of the BMFT stakeholder communities. For these communities, access to healthy natural resources, including rich soils and plentiful, clean water, is essential for their prosperity and well-being. Climate change and ecosystem degradation, agricultural practices, and natural cycles have been altered, causing farmers economic losses and production challenges.  
+
+The restorative and regenerative agriculture program focused on collaborating closely with local communities to promote environmentally sustainable farming practices. Key initiatives include the reforestation of communal areas and agricultural lands, agroforestry systems, and the incorporation of cover crops like Mucuna and Canavalia beans, as well as Inga edulis, all of which are leguminous plants recognized for their ability to enhance soil fertility.  
                     </p>
                     <p>
-                        Additionally, the program advocates for agroforestry and controlled burning as essential farming techniques that foster a harmonious relationship among food production, wildlife conservation, and community well-being. By promoting these practices, the initiative seeks to create a balanced ecosystem where agricultural activities coexist with natural habitats, thereby benefiting both the environment and local populations. Through awareness and support, the program endeavours to empower farmers to adopt these innovative approaches, ultimately leading to a more resilient agricultural landscape that prioritizes sustainability and ecological health.
+                        By fostering awareness and providing support, the program aims to empower farmers to embrace these progressive methods, paving the way for a more resilient agricultural environment that emphasizes sustainability and ecological well-being.  
                     </p>
                 </div>
                 <section className={styles.imageGallery}>
@@ -74,7 +76,11 @@ const AgroProj = () => {
                 reverse={false} 
                 onImageClick={handleImageClick}
             >
-                Cover crops are planted to cover the soil rather than for the purpose of being harvested. They play a crucial role in improving soil health by enhancing soil structure, increasing organic matter, and preventing erosion. Common examples include legumes and grasses, which also contribute to nutrient cycling and weed suppression.
+                Cover crops promoted are two leguminous species Mucuna sp. and Canavalia sp. were purposefully cultivated on separate one-acre plots. These cover crops serve a dual function: enhancing soil health and protecting the soil surface from direct exposure to harsh climatic conditions. By integrating cover crops into farming systems, this nature-based approach supports soil restoration and improves nutrient cycling. 
+
+Canavalia sp. commonly known as Jack bean is a shrubby, annual to short-lived perennial legume characterized by a woody structure and a deep taproot system. Its broad leaves consist of three ovate leaflets, each narrowing to a tapered tip and wedge-shaped at the base. The plant produces smooth flowers in shades of rose, purplish, or white, and its seeds are marked by a distinct brown scar, approximately one-third the length of the seed. Notably, the roots develop nodules that actively fix atmospheric nitrogen, enhancing soil fertility.  
+
+Mucuna bean, scientifically classified as Mucuna sp., is a vigorous leguminous vine valued for its wide-ranging benefits in agriculture and beyond. It features large, broad leaves and striking clusters of flowers that range in color from deep purple to bright pink. The plant produces elongated pods containing nutrient-rich beans known for their high protein content. In addition to their nutritional value, Mucuna beans contribute to sustainable agriculture by enriching soil fertility through nitrogen fixation, making them a valuable component of regenerative and eco-friendly farming systems.  
             </TopicSection>
             <TopicSection
                 title="Inga edulis"
@@ -82,7 +88,7 @@ const AgroProj = () => {
                 reverse={true} 
                 onImageClick={handleImageClick}
             >
-                Inga edulis, also known as the ice cream bean tree, is a leguminous tree that is integral to agroforestry systems. Its leaves are high in nitrogen, which helps in enriching the soil, and it provides shade, reduces soil erosion, and supports biodiversity.
+               Inga edulis is a nitrogen-fixing plant known for its ability to contribute to the restoration of soil health through root nodules containing nitrogen-fixing bacteria and the production of leaf mulch that accumulates and helps to mimic the condition of a natural forest in which organic inputs are constant. The shade of the Inga trees and leaf litter also reduces the soil temperature to the levels found in a natural forest. This practice helps in the restoration and maintenance of soil fertility and weed control and improves soil conditions and soil biodiversity. The Inga alley cropping system is an agricultural method used as a slash-and-mulch method, reducing the need to slash and burn.  
             </TopicSection>
             <TopicSection
                 title="Agroforestry"
@@ -90,7 +96,7 @@ const AgroProj = () => {
                 reverse={false} 
                 onImageClick={handleImageClick}
             >
-                Agroforestry integrates trees and shrubs into crop and animal farming systems to create environmental and economic benefits. This approach enhances biodiversity, improves soil health, and increases farm resilience against climate change.
+                Agroforestry is the deliberate integration of trees and shrubs into agricultural systems—either concurrently or in rotation with crops and/or livestock. This practice involves the strategic planning and management of diverse vegetation to maximize resource efficiency, enhance ecosystem services, and support sustainable land stewardship. Unlike conventional monoculture farming, agroforestry prioritizes ecological diversity and resilience by fostering beneficial interactions among trees, crops, and animals within a unified agroecosystem. This provides refuge for wildlife and supports the conservation of native species, helping maintain ecological balance and ecosystem resilience. Trees enrich the soil through organic matter from leaf litter and root exudates, which improves soil structure, water retention, and nutrient cycling, enhancing soil fertility and reducing dependence on synthetic fertilizers and pesticides.  
             </TopicSection>
             <TopicSection
                 title="Reforestation"
@@ -98,7 +104,7 @@ const AgroProj = () => {
                 reverse={true} 
                 onImageClick={handleImageClick}
             >
-                Controlled or prescribed burns are a land management tool used to reduce the risk of uncontrolled wildfires, improve soil health, and promote new plant growth. This technique helps in maintaining ecological balance by mimicking natural fire cycles.
+                Restoring the lost forests is a shared priority for local communities, the Belize Maya Forest Trust (BMFT), and the government of Belize. This vital initiative aims to address the challenges posed by climate change while rejuvenating the ecosystem to preserve essential ecological services that benefit all. In 2024, the BMFT successfully planted over 6,000 native timber and fruit trees, including species like cedar, mahogany, Guanacaste, coffee, cacao, and Pacaya palm, across five different stakeholder communities. Each tree planting event not only contributed to reforestation but also fostered community engagement, as farmers, women, children, and youth participated in hands-on learning about the best practices for transplanting and nurturing these plants, ensuring their healthy growth.
             </TopicSection>
             <TopicSection
                 title="Workshops and Training"
